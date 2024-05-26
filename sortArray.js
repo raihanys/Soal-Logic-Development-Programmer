400c9451ace610a267a80271721a3bcf83ddccff
Raihan Yusuf Sa'ari <87800532+raihanys@users.noreply.github.com>
2024-05-26 18:46:57 +0700

    Update sortArray.js

--- sortArray.js
+++ sortArray.js
@@ -1,10 +1,3 @@
-/* 
-Soal
-
-Urutkan array berikut [12,9,30,”A”,”M”,99,82,”J”,”N”,”B”] dengan urutan abjad
-di depan dan angka di belakang, contoh [“A”, “B”,”J”, “M”, “N”, 9, 12, 30, 82, 99] 
-*/
-
 // Tahap Deklarasi isi Array
 const array = [12, 9, 30, "A", "M", 99, 82, "J", "N", "B"];
 
